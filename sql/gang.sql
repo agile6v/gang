f34b8c1d3be49d4e1cb634099e6aaec2fc3ab5d8
agile6v <agile6v@agile6v.com>
2017-05-23 22:44:28 +0800

    add host_group table in the sql

--- sql/gang.sql
+++ sql/gang.sql
@@ -17,11 +17,26 @@ CREATE TABLE `task` (
     UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='crontab';
 
+
 DROP TABLE IF EXISTS `host`;
 CREATE TABLE `host` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'incremental id',
-  `ip` varchar(128) NOT NULL DEFAULT '' COMMENT 'ip address',
+  `host` varchar(128) NOT NULL DEFAULT '' COMMENT 'ip address or hostname',
   `task_id` int(11) NOT NULL DEFAULT '0' COMMENT 'task on this host',
+  `group_id` int(11) NOT NULL DEFAULT '1' COMMENT 'host group id',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `ip_task` (`ip`,`task_id`)
+  UNIQUE KEY `host_task` (`host`,`task_id`),
+  KEY `task_idx` (`task_id`),
+  KEY `host_idx` (`host`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='the mapping table of host & task ';
+
+
+DROP TABLE IF EXISTS `host_group`;
+CREATE TABLE `host_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'incremental id',
+  `name` varchar(128) NOT NULL DEFAULT '' COMMENT 'group name',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='host group';
+
+INSERT INTO `host_group` (`id`, `name`) VALUES (1, 'default group');
+
